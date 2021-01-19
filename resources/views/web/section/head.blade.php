ab47e4077dfe53d6dd711cb01f34465e31bce76b
rmehdi555 <rezaie.mehdi555@gmail.com>
2021-01-19 16:37:17 +0330

    pull of main

--- resources/views/web/section/head.blade.php
+++ resources/views/web/section/head.blade.php
@@ -64,6 +64,7 @@ $locale = App::getLocale();
         img{
             max-width:100%;
         }
+
     </style>
     <!-- JavaScript -->
     <script type="text/javascript" src="{{asset('web/2020/assets/js/jquery-3.3.1.min.js')}}"></script>
